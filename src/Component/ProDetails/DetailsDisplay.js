0b1b74d90ebc1a724c4d66b2e3c37aa13f6d473a
Umesh <mathulkar98@gmail.com>
2023-02-20 14:05:16 +0530

    updated detailsPage

--- src/Component/ProDetails/DetailsDisplay.js
+++ src/Component/ProDetails/DetailsDisplay.js
@@ -1,32 +1,87 @@
 import React from "react";
 import './DetailsDisplay.css'
+import { useEffect, useState } from "react";
 
-const DetailsDisplay=(props)=>{
+import { Link } from "react-router-dom";
+const catUrl = "https://winkart.onrender.com/category"
+const DetailsDisplay = (props) => {
 
 
-const renderDetails=({passDetails})=>{
-    if(passDetails){
-        return (
-            <div className="detailsMain">
-                <div className="row">
-                    <div className="detailsImg col-lg-6">
-                        <img src={passDetails[0]['image']}/>
-                    </div>
-                    <div className="col-lg-6">
-                        <h2>{passDetails[0]['product_name']}</h2>
-                        <p>{passDetails[0]['description']}</p>
-                        <button className="btn btn-success mt-4">Buy Now {passDetails[0]['company']} {passDetails[0]['subCat_name']}</button>
-                    </div>
+
+    const RenderTab = (subTab) => {
+        if (subTab) {
+            return (
+                <div>
+                    {subTab.map((item) => {
+                        return (
+                            <div className="proAligns">
+                                <li className="nav-item">
+
+                                    <Link to={`/allProd/${item.category_id}`} className="nav-link active" aria-current="page" href="#">{item.category}</Link>
+
+                                </li>
+                            </div>
+
+
+
+                        )
+                    })}
                 </div>
-        
+            )
+        }
+
+    }
+
+    const RenderSubTab = () => {
+        const [subTab, setSubTab] = useState();
+        useEffect(() => {
+            fetch(catUrl, { method: 'GET' })
+                .then((res) => res.json())
+                .then((data) => {
+                    setSubTab(data)
+                })
+        })
+
+        return (
+            <div>
+                <ul className="nav mt-2 justify-content-center">
+                    {RenderTab(subTab)}
+                </ul>
+                <hr/>
             </div>
         )
     }
-}
 
 
-    return(
+
+    const renderDetails = ({ passDetails }) => {
+        if (passDetails) {
+            return (
+                <div className="detailsMain">
+
+                    <div className="row">
+                        <div className="detailsImg container col-lg-6">
+                            <img src={passDetails[0]['image']} />
+                        </div>
+                        <div className="col-lg-6">
+                            <div className="container detailsInfo">
+                                <h2 className="mt-3">{passDetails[0]['product_name']}</h2>
+                                <p>{passDetails[0]['description']}</p>
+                                <h4>Just Rs. {passDetails[0]['Price']}</h4>
+                                <button className="btn btn-success mt-4">Buy Now {passDetails[0]['company']} {passDetails[0]['subCat_name']}</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            )
+        }
+    }
+
+
+    return (
         <div>
+            {RenderSubTab()}
             {renderDetails(props)}
         </div>
     )
